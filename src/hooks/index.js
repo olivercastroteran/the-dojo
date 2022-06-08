e490dabaf2b0136af5e70e8da06b7f45a48b50b4
oliver <olivercastroteran@gmail.com>
2022-06-08 02:24:00 -0400

    Fetching individual project and rendering it on ProjectSummary component

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,6 @@
 export { default as useAuthContext } from './useAuthContext';
 export { default as useCollection } from './useCollection';
+export { default as useDocument } from './useDocument';
 export { default as useFirestore } from './useFirestore';
 export { default as useLogin } from './useLogin';
 export { default as useLogout } from './useLogout';
---
+++ src/views/Project/components/index.js
@@ -0,0 +1,1 @@
+export { default as ProjectSummary } from './ProjectSummary/ProjectSummary';
